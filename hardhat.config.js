550d742be7ed896bf6ba88ee6c804e7474eb87af
Anukul Pandey <anukul.030601@gmail.com>
2023-12-28 14:19:55 +0530

    chore: init

---
+++ hardhat.config.js
@@ -0,0 +1,47 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+require("@reef-chain/hardhat-reef");
+require("@openzeppelin/hardhat-upgrades");
+
+module.exports = {
+  defaultNetwork: "reef_testnet",
+  networks: {
+    localhost: {
+      url: "http://localhost:8545", // uses account 0 of the hardhat node to deploy
+    },
+    reef_testnet: {
+      url: "wss://rpc-testnet.reefscan.com/ws",
+      scanUrl: "https://api-testnet.reefscan.com",
+      seeds: {
+        testnet_account: process.env.MNEMONIC_TESTNET|| "expire pepper arena virus budget craft industry hawk devote major symbol labor",
+      },
+    },
+    reef_mainnet: {
+      url: "wss://rpc.reefscan.com/ws",
+      scanUrl: "https://api.reefscan.com",
+      seeds: {
+        mainnet_account: process.env.MNEMONIC_MAINNET || "",
+      },
+    },
+  },
+  plugins: ["solidity-coverage"],
+  solidity: {
+    compilers: [
+      {
+        version: "0.7.6",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+};
